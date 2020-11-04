2793574585d1eae731df3d430f787c95f6574ec0
lydiadiffanisiregar <lydiadiffanii@gmail.com>
2020-11-04 12:45:16 +0700

    Update README.md

--- tugas_pertemuan5.py
+++ tugas_pertemuan5.py
@@ -28,3 +28,13 @@ dob=2020-year
 
 print("\n\n Assalamualaikum. ")
 print(f"Let me introduce my self, my name is {fullname}, but you can call me {nickname}, my NPM {npm}, I was born in {pob} and iam {dob} years old, I am very glad if you want to invite my house in {address}, So don't forget to call me before with the number {phone}, \n\n Thanks you ")
+ * Untuk source code berikutnya adalah inputan atau membuat variable. seperti syntax dibawah ini :
+ ```python
+fullname=input()
+```
+Keterangan : <br>
+>Variable adalah sebuah wadah penyimpanan data pada program yang akan akan digunakan selama program itu berjalan. yang berfungsi sebagai variable dalam source code diatas adalah **fullname** . <br>
+>Fungsi **input()** adalah untuk memasukan nilai dari layar console di command prompt, lalu kemudian mengembalikan nilai saat kita menekan tombol enter *(newline)*<br>
+ *(newline)*<br>
+
+ ![gitpush](gambar/)
